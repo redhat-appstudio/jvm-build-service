c0f8aabd41be5950537992489efd5ef92ba54f01
Marek Goldmann <mgoldman@redhat.com>
2022-04-07 17:15:41 +0200

    feat(cache): health checks, more docs

---
+++ cache/src/test/java/com/github/stuartwdouglas/mavenproxy/test/health/HealthTest.java
@@ -0,0 +1,20 @@
+package com.github.stuartwdouglas.mavenproxy.test.health;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+
+@QuarkusTest
+public class HealthTest {
+    @Test
+    public void testMainHealthCheck() {
+
+        RestAssured.given()
+                .when().get("/q/health")
+                .then()
+                .statusCode(200)
+                .body("status", CoreMatchers.equalTo("UP"));
+    }
+}
