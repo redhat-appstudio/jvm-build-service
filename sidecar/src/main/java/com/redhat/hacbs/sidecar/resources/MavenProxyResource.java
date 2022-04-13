609df50251a937fc1e44770d76211a1d4ee8e7bd
Stuart Douglas <stuart.w.douglas@gmail.com>
2022-04-13 11:47:13 +1000

    Add initial support for S3 deployment

--- sidecar/src/main/java/com/redhat/hacbs/sidecar/resources/MavenProxyResource.java
+++ sidecar/src/main/java/com/redhat/hacbs/sidecar/resources/MavenProxyResource.java
@@ -1,8 +1,6 @@
 package com.redhat.hacbs.sidecar.resources;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
@@ -11,6 +9,9 @@ import com.redhat.hacbs.sidecar.services.RemoteClient;
 
 import io.quarkus.logging.Log;
 import io.smallrye.common.annotation.Blocking;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Path("/maven2")
 @Blocking
@@ -18,11 +19,19 @@ public class MavenProxyResource {
 
     final RemoteClient remoteClient;
     final String buildPolicy;
+    final S3Client client;
+    final String deploymentBucket;
+    final String deploymentPrefix;
 
     public MavenProxyResource(@RestClient RemoteClient remoteClient,
-            @ConfigProperty(name = "build-policy") String buildPolicy) {
+            @ConfigProperty(name = "build-policy") String buildPolicy, S3Client client,
+            @ConfigProperty(name = "deployment-bucket") String deploymentBucket,
+            @ConfigProperty(name = "deployment-prefix") String deploymentPrefix) {
         this.remoteClient = remoteClient;
         this.buildPolicy = buildPolicy;
+        this.client = client;
+        this.deploymentBucket = deploymentBucket;
+        this.deploymentPrefix = deploymentPrefix;
         Log.infof("Constructing resource manager with build policy %s", buildPolicy);
     }
 
@@ -31,14 +40,50 @@ public class MavenProxyResource {
     public byte[] get(@PathParam("group") String group, @PathParam("artifact") String artifact,
             @PathParam("version") String version, @PathParam("target") String target) throws Exception {
         Log.infof("Retrieving artifact %s/%s/%s/%s", group, artifact, version, target);
-        return remoteClient.get(buildPolicy, group, artifact, version, target);
+        try {
+            return remoteClient.get(buildPolicy, group, artifact, version, target);
+        } catch (WebApplicationException e) {
+            if (e.getResponse().getStatus() == 404) {
+                throw new NotFoundException();
+            }
+            throw e;
+        }
     }
 
     @GET
-    @Path("{group:.*?}/{target}")
-    public byte[] get(@PathParam("group") String group, @PathParam("target") String target) throws Exception {
-        Log.infof("Retrieving file %s/%s", group, target);
-        return remoteClient.get(buildPolicy, group, target);
+    @Path("{group:.*?}/maven-metadata.xml{hash:.*?}")
+    public byte[] get(@PathParam("group") String group, @PathParam("hash") String hash) throws Exception {
+        Log.infof("Retrieving file %s/maven-metadata.xml%s", group, hash);
+        try {
+            return remoteClient.get(buildPolicy, group, hash);
+        } catch (WebApplicationException e) {
+            if (e.getResponse().getStatus() == 404) {
+                throw new NotFoundException();
+            }
+            throw e;
+        }
     }
 
+    //TODO: deployment is really hacked up at the moment, this needs to be modified to be an atomic operation
+    //this is ok for the PoC phase, but needs to be fixed
+    //We will likely either want to use the nexus plugin and emulate the nexus API, or write out own plugin
+    //alternatively we could deploy to a file system location and have a task that does the deployment from there
+    @PUT
+    @Path("{group:.*?}/{artifact}/{version}/{target}")
+    public void deply(@PathParam("group") String group, @PathParam("artifact") String artifact,
+            @PathParam("version") String version, @PathParam("target") String target, byte[] data) throws Exception {
+        Log.errorf("DEPLOYING artifact %s/%s/%s/%s", group, artifact, version, target);
+
+        String fullTarget = deploymentPrefix + "/" + group + "/" + artifact + "/" + version + "/" + target;
+        client.putObject(PutObjectRequest.builder().bucket(deploymentBucket).key(fullTarget).build(),
+                RequestBody.fromBytes(data));
+    }
+
+    @PUT
+    @Path("{group:.*?}/maven-metadata.xml{hash:.*?}")
+    public void get(@PathParam("group") String group, @PathParam("hash") String hash, byte[] data) throws Exception {
+        String fullTarget = deploymentPrefix + "/" + group + "/maven-metadata.xml" + hash;
+        client.putObject(PutObjectRequest.builder().bucket(deploymentBucket).key(fullTarget).build(),
+                RequestBody.fromBytes(data));
+    }
 }
