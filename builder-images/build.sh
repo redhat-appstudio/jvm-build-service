81326b6c48a433b990457af60fb42200c67b7d43
Stuart Douglas <stuart.w.douglas@gmail.com>
2022-05-31 12:51:03 +1000

    tmp

--- builder-images/build.sh
+++ builder-images/build.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-cekit --descriptor  builder-image.yaml build podman --no-squash
-cekit --descriptor   builder-image.yaml build  --overrides-file jdk11.yaml podman --no-squash
-cekit --descriptor  builder-image.yaml build  --overrides-file jdk17.yaml podman --no-squash
+sed "s/QUAY_REPO/$1/" builder-image-template.yaml | sed "s/SNAPSHOT/$2/" >builder-image.generated.yaml
+sed "s/QUAY_REPO/$1/" jdk11-template.yaml | sed "s/SNAPSHOT/$2/" >jdk11.generated.yaml
+sed "s/QUAY_REPO/$1/" jdk17-template.yaml | sed "s/SNAPSHOT/$2/" >jdk17.generated.yaml
 
-podman push quay.io/sdouglas/hacbs-jdk17-builder:latest
-podman push quay.io/sdouglas/hacbs-jdk11-builder:latest
-podman push quay.io/sdouglas/hacbs-jdk8-builder:latest
+cekit --descriptor builder-image.generated.yaml build "$3" --no-squash
+cekit --descriptor builder-image.generated.yaml build  --overrides-file jdk11.generated.yaml "$3" --no-squash
+cekit --descriptor builder-image.generated.yaml build  --overrides-file jdk17.generated.yaml "$3" --no-squash
+
+echo quay.io/$1/hacbs-jdk17-builder:$2
+$3 push quay.io/$1/hacbs-jdk11-builder:$2
+$3 push quay.io/$1/hacbs-jdk17-builder:$2
+$3 push quay.io/$1/hacbs-jdk8-builder:$2
