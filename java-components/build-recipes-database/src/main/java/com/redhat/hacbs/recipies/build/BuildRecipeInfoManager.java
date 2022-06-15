9b57bc8c812916f9fca4db2d4ab5ad39b40eaedb
Stuart Douglas <stuart.w.douglas@gmail.com>
2022-06-15 13:34:24 +1000

    Don't fail on unkown properties
    This allows existing images to keep working if the database is extended.

--- java-components/build-recipes-database/src/main/java/com/redhat/hacbs/recipies/build/BuildRecipeInfoManager.java
+++ java-components/build-recipes-database/src/main/java/com/redhat/hacbs/recipies/build/BuildRecipeInfoManager.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.redhat.hacbs.recipies.RecipeManager;
@@ -11,6 +12,7 @@ import com.redhat.hacbs.recipies.RecipeManager;
 public class BuildRecipeInfoManager implements RecipeManager<BuildRecipeInfo> {
 
     private static final ObjectMapper MAPPER = new ObjectMapper(new YAMLFactory())
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
             .setSerializationInclusion(JsonInclude.Include.NON_DEFAULT);
 
     @Override
