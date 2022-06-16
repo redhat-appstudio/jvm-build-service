d47e622cd7fb5dc502c74da89934cd1920f96e75
Stuart Douglas <stuart.w.douglas@gmail.com>
2022-06-16 18:12:24 +1000

    Builder image improvements

--- builder-images/build.sh
+++ builder-images/build.sh
@@ -4,11 +4,11 @@ sed "s/QUAY_REPO/$1/" builder-image-template.yaml | sed "s/SNAPSHOT/$2/" >builde
 sed "s/QUAY_REPO/$1/" jdk11-template.yaml | sed "s/SNAPSHOT/$2/" >jdk11.generated.yaml
 sed "s/QUAY_REPO/$1/" jdk17-template.yaml | sed "s/SNAPSHOT/$2/" >jdk17.generated.yaml
 
-cekit --descriptor builder-image.generated.yaml build "$3" --no-squash
 cekit --descriptor builder-image.generated.yaml build  --overrides-file jdk11.generated.yaml "$3" --no-squash
-cekit --descriptor builder-image.generated.yaml build  --overrides-file jdk17.generated.yaml "$3" --no-squash
-
-echo quay.io/$1/hacbs-jdk17-builder:$2
 $3 push quay.io/$1/hacbs-jdk11-builder:$2
-$3 push quay.io/$1/hacbs-jdk17-builder:$2
+
+cekit --descriptor builder-image.generated.yaml build "$3" --no-squash
 $3 push quay.io/$1/hacbs-jdk8-builder:$2
+
+cekit --descriptor builder-image.generated.yaml build  --overrides-file jdk17.generated.yaml "$3" --no-squash
+$3 push quay.io/$1/hacbs-jdk17-builder:$2
