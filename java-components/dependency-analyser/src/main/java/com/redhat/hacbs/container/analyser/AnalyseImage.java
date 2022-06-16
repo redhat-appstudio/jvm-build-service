cd3a571601bd8244ab807d3e5da3a9025e590574
Stuart Douglas <stuart.w.douglas@gmail.com>
2022-06-16 16:48:20 +1000

    Allow SBOM generation for image based analysis

--- java-components/dependency-analyser/src/main/java/com/redhat/hacbs/container/analyser/AnalyseImage.java
+++ java-components/dependency-analyser/src/main/java/com/redhat/hacbs/container/analyser/AnalyseImage.java
@@ -17,6 +17,7 @@ import javax.inject.Singleton;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 
+import com.google.cloud.tools.jib.api.Credential;
 import com.google.cloud.tools.jib.api.DescriptorDigest;
 import com.google.cloud.tools.jib.api.ImageReference;
 import com.google.cloud.tools.jib.api.InvalidImageReferenceException;
@@ -30,14 +31,15 @@ import com.google.cloud.tools.jib.image.json.V21ManifestTemplate;
 import com.google.cloud.tools.jib.registry.ManifestAndDigest;
 import com.google.cloud.tools.jib.registry.RegistryClient;
 import com.google.cloud.tools.jib.registry.credentials.CredentialRetrievalException;
-import com.redhat.hacbs.classfile.tracker.ClassFileTracker;
+import com.redhat.hacbs.classfile.tracker.NoCloseInputStream;
+import com.redhat.hacbs.classfile.tracker.TrackingData;
 
 import io.quarkus.logging.Log;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "image")
 @Singleton
-public class AnalyseImage implements Runnable {
+public class AnalyseImage extends AnalyserBase {
 
     @CommandLine.Parameters(index = "0")
     String image;
@@ -45,69 +47,36 @@ public class AnalyseImage implements Runnable {
     @CommandLine.Option(names = { "--base-image", "-b" }, required = false)
     Optional<String> baseImage;
 
-    @CommandLine.Option(names = "--allowed-sources", defaultValue = "redhat")
-    Set<String> allowedSources;
-
     @Inject
     RebuildService rebuild;
 
-    @Override
-    public void run() {
-        try {
-            Set<DescriptorDigest> layersToProcess = new HashSet<>();
-            RegistryClient client = extractLayers(image, layersToProcess::add);
-            Log.infof("Processing image %s", image);
-            if (baseImage.isPresent()) {
-                Log.infof("Processing base image %s", image);
-                extractLayers(baseImage.get(), layersToProcess::remove);
-            }
-            Log.infof("Processing layers to extract: %s", layersToProcess);
-            Set<String> gavs = new HashSet<>();
-            for (var layer : layersToProcess) {
-                var blob = client.pullBlob(layer, s -> {
-                }, s -> {
-                });
-                Path targetFile = Files.createTempFile("layer", "layer");
+    void doAnalysis(Set<String> gavs, Set<TrackingData> trackingData) throws Exception {
+        Set<DescriptorDigest> layersToProcess = new HashSet<>();
+        RegistryClient client = extractLayers(image, layersToProcess::add);
+        Log.infof("Processing image %s", image);
+        if (baseImage.isPresent()) {
+            Log.infof("Processing base image %s", image);
+            extractLayers(baseImage.get(), layersToProcess::remove);
+        }
+        Log.infof("Processing layers to extract: %s", layersToProcess);
+        for (var layer : layersToProcess) {
+            var blob = client.pullBlob(layer, s -> {
+            }, s -> {
+            });
+            Path targetFile = Files.createTempFile("layer", "layer");
 
-                try (OutputStream out = Files.newOutputStream(targetFile)) {
-                    blob.writeTo(out);
-                }
-                try (InputStream in = Files.newInputStream(targetFile)) {
-                    GZIPInputStream inputStream = new GZIPInputStream(in);
-                    TarArchiveInputStream tarArchiveInputStream = new TarArchiveInputStream(inputStream);
-                    for (TarArchiveEntry entry = tarArchiveInputStream
-                            .getNextTarEntry(); entry != null; entry = tarArchiveInputStream.getNextTarEntry()) {
-                        if (entry.getName().endsWith(".class")) {
-                            Log.debugf("Processing %s from layer %s", entry.getName(), layer.getHash());
-                            var data = ClassFileTracker.readTrackingInformationFromClass(tarArchiveInputStream.readAllBytes());
-                            if (data != null) {
-                                if (!allowedSources.contains(data.source)) {
-                                    Log.debugf("Found GAV %s in %s from layer %s", data.gav, entry.getName(), layer.getHash());
-                                    gavs.add(data.gav);
-                                }
-                            }
-                        } else if (entry.getName().endsWith(".jar")) {
-                            Log.debugf("Processing %s from layer %s", entry.getName(), layer.getHash());
-                            var jarData = ClassFileTracker.readTrackingDataFromJar(tarArchiveInputStream.readAllBytes(),
-                                    entry.getName());
-                            for (var data : jarData) {
-                                if (data != null) {
-                                    if (allowedSources.contains(data.source)) {
-                                        Log.debugf("Found GAV %s in %s from layer %s", data.gav, entry.getName(),
-                                                layer.getHash());
-                                        gavs.add(data.gav);
-                                    }
-                                }
-                            }
-                        }
-                    }
+            try (OutputStream out = Files.newOutputStream(targetFile)) {
+                blob.writeTo(out);
+            }
+            try (InputStream in = Files.newInputStream(targetFile)) {
+                GZIPInputStream inputStream = new GZIPInputStream(in);
+                TarArchiveInputStream tarArchiveInputStream = new TarArchiveInputStream(inputStream);
+                for (TarArchiveEntry entry = tarArchiveInputStream
+                        .getNextTarEntry(); entry != null; entry = tarArchiveInputStream.getNextTarEntry()) {
+                    Log.debugf("Processing %s from layer %s", entry.getName(), layer.getHash());
+                    handleFile(entry.getName(), new NoCloseInputStream(tarArchiveInputStream), trackingData, gavs);
                 }
             }
-
-            rebuild.rebuild(gavs);
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
     }
 
@@ -119,7 +88,9 @@ public class AnalyseImage implements Runnable {
         RegistryClient.Factory factory = RegistryClient.factory(new EventHandlers.Builder().build(),
                 imageReference.getRegistry(),
                 imageReference.getRepository(), new FailoverHttpClient(false, false, s -> System.out.println(s.getMessage())));
-        factory.setCredential(credentialRetrieverFactory.dockerConfig().retrieve().get());
+
+        Optional<Credential> optionalCredential = credentialRetrieverFactory.dockerConfig().retrieve();
+        optionalCredential.ifPresent(factory::setCredential);
         RegistryClient registryClient = factory.newRegistryClient();
 
         ManifestAndDigest<ManifestTemplate> result = registryClient.pullManifest(imageReference.getQualifier());
--- java-components/dependency-analyser/src/main/java/com/redhat/hacbs/container/analyser/AnalysePath.java
+++ java-components/dependency-analyser/src/main/java/com/redhat/hacbs/container/analyser/AnalysePath.java
@@ -1,27 +1,16 @@
 package com.redhat.hacbs.container.analyser;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.inject.Inject;
 import javax.inject.Singleton;
 
-import org.cyclonedx.BomGeneratorFactory;
-import org.cyclonedx.CycloneDxSchema;
-import org.cyclonedx.generators.json.BomJsonGenerator;
-import org.cyclonedx.model.Bom;
-import org.cyclonedx.model.Component;
-
-import com.redhat.hacbs.classfile.tracker.ClassFileTracker;
 import com.redhat.hacbs.classfile.tracker.TrackingData;
 
 import io.quarkus.logging.Log;
@@ -29,97 +18,20 @@ import picocli.CommandLine;
 
 @CommandLine.Command(name = "path")
 @Singleton
-public class AnalysePath implements Runnable {
+public class AnalysePath extends AnalyserBase implements Runnable {
 
     @CommandLine.Parameters
     List<Path> paths;
 
-    @CommandLine.Option(names = "--allowed-sources", defaultValue = "redhat,rebuilt")
-    Set<String> allowedSources;
-
-    @Inject
-    RebuildService rebuild;
-
-    @CommandLine.Option(names = "-s")
-    Path sbom;
-
-    @CommandLine.Option(names = "-c")
-    Path dependenciesCount;
-
-    @CommandLine.Option(names = "-u")
-    Path untrustedDependenciesCount;
-
-    @Override
-    public void run() {
-        try {
-            Log.infof("Root paths %s", paths);
-            Set<String> gavs = new HashSet<>();
-            Set<TrackingData> trackingData = new HashSet<>();
-            for (var path : paths) {
-                Files.walkFileTree(path, new SimpleFileVisitor<>() {
-                    @Override
-                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                        if (file.getFileName().toString().endsWith(".class")) {
-                            Log.debugf("Processing %s", file);
-                            var data = ClassFileTracker.readTrackingInformationFromClass(Files.readAllBytes(file));
-                            if (data != null) {
-                                trackingData.add(data);
-                                if (!allowedSources.contains(data.source)) {
-                                    Log.debugf("Found GAV %s in %s", data.gav, file);
-                                    gavs.add(data.gav);
-                                }
-                            }
-                        } else if (file.getFileName().toString().endsWith(".jar")) {
-                            Log.debugf("Processing %s", file);
-                            var jarData = ClassFileTracker.readTrackingDataFromJar(Files.readAllBytes(file),
-                                    file.getFileName().toString());
-                            trackingData.addAll(jarData);
-                            for (var data : jarData) {
-                                if (data != null) {
-                                    if (!allowedSources.contains(data.source)) {
-                                        Log.debugf("Found GAV %s in %s", data.gav, file);
-                                        gavs.add(data.gav);
-                                    }
-                                }
-                            }
-                        }
-                        return FileVisitResult.CONTINUE;
-                    }
-                });
-            }
-            rebuild.rebuild(gavs);
-            if (dependenciesCount != null) {
-                Files.writeString(dependenciesCount, trackingData.size() + "");
-            }
-            if (untrustedDependenciesCount != null) {
-                Files.writeString(untrustedDependenciesCount, gavs.size() + "");
-            }
-
-            //now build a cyclone DX bom file
-            final Bom bom = new Bom();
-            bom.setComponents(new ArrayList<>());
-            for (var i : trackingData) {
-                var split = i.gav.split(":");
-                String group = split[0];
-                String name = split[1];
-                String version = split[2];
-                Component component = new Component();
-                component.setType(Component.Type.LIBRARY);
-                component.setGroup(group);
-                component.setName(name);
-                component.setVersion(version);
-                component.setPublisher(i.source);
-                bom.getComponents().add(component);
-            }
-            BomJsonGenerator generator = BomGeneratorFactory.createJson(CycloneDxSchema.Version.VERSION_14, bom);
-            String sbom = generator.toJsonString();
-            Log.infof("Generated SBOM:\n%s", sbom);
-            if (this.sbom != null) {
-                Files.writeString(this.sbom, sbom, StandardCharsets.UTF_8);
-            }
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+    void doAnalysis(Set<String> gavs, Set<TrackingData> trackingData) throws IOException {
+        Log.infof("Root paths %s", paths);
+        for (var path : paths) {
+            Files.walkFileTree(path, new SimpleFileVisitor<>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    return handleFile(file.getFileName().toString(), Files.newInputStream(file), trackingData, gavs);
+                }
+            });
         }
     }
 
