f9882f4312147f18a6550d580dff2bba2f18bd34
gabemontero <gmontero@redhat.com>
2022-08-08 17:15:27 -0400

    rebase periodic on refactored e2e base

--- openshift-with-appstudio-test/e2e/periodic_test.go
+++ openshift-with-appstudio-test/e2e/periodic_test.go
@@ -7,257 +7,21 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
 	"os"
 	"path/filepath"
 	"testing"
 	"time"
 
-	projectv1 "github.com/openshift/api/project/v1"
 	"github.com/redhat-appstudio/jvm-build-service/pkg/apis/jvmbuildservice/v1alpha1"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
-	utilrand "k8s.io/apimachinery/pkg/util/rand"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"knative.dev/pkg/apis"
 )
 
-//TODO move all the *2 funcs/const to a separate/common go file, or separate package under e2e, to share with jvm_build_service_test.go
-
-const (
-	testNamespace2          = "jvm-build-service-test-namespace-"
-	maxNameLength2          = 63
-	randomLength2           = 5
-	maxGeneratedNameLength2 = maxNameLength2 - randomLength2
-)
-
-
-type testArgs2 struct {
-	t  *testing.T
-	ns string
-
-	timeout  time.Duration
-	interval time.Duration
-
-	gitClone *v1beta1.Task
-	maven    *v1beta1.Task
-	pipeline *v1beta1.Pipeline
-	run      *v1beta1.PipelineRun
-}
-
-func (ta *testArgs2) Logf(msg string) {
-	ta.t.Logf(fmt.Sprintf("time: %s: %s", time.Now().String(), msg))
-}
-
-func generateName2(base string) string {
-	if len(base) > maxGeneratedNameLength2 {
-		base = base[:maxGeneratedNameLength2]
-	}
-	return fmt.Sprintf("%s%s", base, utilrand.String(randomLength2))
-}
-
-func dumpPods2(ta *testArgs2, namespace string) {
-	podClient := kubeClient.CoreV1().Pods(namespace)
-	podList, err := podClient.List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		ta.Logf(fmt.Sprintf("error list pods %s", err.Error()))
-		return
-	}
-	ta.Logf(fmt.Sprintf("dumpPods2 have %d items in list", len(podList.Items)))
-	for _, pod := range podList.Items {
-		ta.Logf(fmt.Sprintf("dumpPods2 looking at pod %s in phase %s", pod.Name, pod.Status.Phase))
-
-		for _, container := range pod.Spec.Containers {
-			req := podClient.GetLogs(pod.Name, &corev1.PodLogOptions{Container: container.Name})
-			readCloser, err := req.Stream(context.TODO())
-			if err != nil {
-				ta.Logf(fmt.Sprintf("error getting pod logs for container %s: %s", container.Name, err.Error()))
-				continue
-			}
-			b, err := ioutil.ReadAll(readCloser)
-			if err != nil {
-				ta.Logf(fmt.Sprintf("error reading pod stream %s", err.Error()))
-				continue
-			}
-			podLog := string(b)
-			ta.Logf(fmt.Sprintf("pod logs for container %s in pod %s:  %s", container.Name, pod.Name, podLog))
-
-		}
-
-	}
-}
-
-func dumpBadEvents2(ta *testArgs2) {
-	eventClient := kubeClient.EventsV1().Events(ta.ns)
-	eventList, err := eventClient.List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		ta.Logf(fmt.Sprintf("error listing events: %s", err.Error()))
-		return
-	}
-	ta.Logf(fmt.Sprintf("dumpBadEvents2 have %d items in total list", len(eventList.Items)))
-	for _, event := range eventList.Items {
-		if event.Type == corev1.EventTypeNormal {
-			continue
-		}
-		ta.Logf(fmt.Sprintf("non-normal event reason %s about obj %s:%s message %s", event.Reason, event.Regarding.Kind, event.Regarding.Name, event.Note))
-	}
-}
-
-func dumpNodes2(ta *testArgs2) {
-	nodeClient := kubeClient.CoreV1().Nodes()
-	nodeList, err := nodeClient.List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		ta.Logf(fmt.Sprintf("error listin nodes: %s", err.Error()))
-		return
-	}
-	ta.Logf(fmt.Sprintf("dumpNodes2 found %d nodes in list, but only logging worker nodes", len(nodeList.Items)))
-	for _, node := range nodeList.Items {
-		_, ok := node.Labels["node-role.kubernetes.io/master"]
-		if ok {
-			continue
-		}
-		if node.Status.Allocatable.Cpu() == nil {
-			ta.Logf(fmt.Sprintf("Node %s does not have allocatable cpu", node.Name))
-			continue
-		}
-		if node.Status.Allocatable.Memory() == nil {
-			ta.Logf(fmt.Sprintf("Node %s does not have allocatable mem", node.Name))
-			continue
-		}
-		if node.Status.Capacity.Cpu() == nil {
-			ta.Logf(fmt.Sprintf("Node %s does not have capacity cpu", node.Name))
-			continue
-		}
-		if node.Status.Capacity.Memory() == nil {
-			ta.Logf(fmt.Sprintf("Node %s does not have capacity mem", node.Name))
-			continue
-		}
-		alloccpu := node.Status.Allocatable.Cpu()
-		allocmem := node.Status.Allocatable.Memory()
-		capaccpu := node.Status.Capacity.Cpu()
-		capacmem := node.Status.Capacity.Memory()
-		ta.Logf(fmt.Sprintf("Node %s allocatable CPU %s allocatable mem %s capacity CPU %s capacitymem %s",
-			node.Name,
-			alloccpu.String(),
-			allocmem.String(),
-			capaccpu.String(),
-			capacmem.String()))
-	}
-}
-
-func decodeBytesToTektonObjbytes2(bytes []byte, obj runtime.Object, ta *testArgs2) runtime.Object {
-	decodingScheme := runtime.NewScheme()
-	utilruntime.Must(v1beta1.AddToScheme(decodingScheme))
-	decoderCodecFactory := serializer.NewCodecFactory(decodingScheme)
-	decoder := decoderCodecFactory.UniversalDecoder(v1beta1.SchemeGroupVersion)
-	err := runtime.DecodeInto(decoder, bytes, obj)
-	if err != nil {
-		debugAndFailTest2(ta, err.Error())
-	}
-	return obj
-}
-
-func streamRemoteYamlToTektonObj2(url string, obj runtime.Object, ta *testArgs2) runtime.Object {
-	resp, err := http.Get(url)
-	if err != nil {
-		debugAndFailTest2(ta, err.Error())
-	}
-	defer resp.Body.Close()
-	bytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		debugAndFailTest2(ta, err.Error())
-	}
-	return decodeBytesToTektonObjbytes2(bytes, obj, ta)
-}
-
-func setup2(t *testing.T, ta *testArgs2) *testArgs2 {
-	if ta == nil {
-		ta = &testArgs2{
-			t:        t,
-			timeout:  time.Minute * 10,
-			interval: time.Second * 15,
-		}
-	}
-	setupClients(ta.t)
-
-	if len(ta.ns) == 0 {
-		ta.ns = generateName2(testNamespace2)
-		_, err := projectClient.ProjectV1().ProjectRequests().Create(context.Background(), &projectv1.ProjectRequest{
-			ObjectMeta: metav1.ObjectMeta{Name: ta.ns},
-		}, metav1.CreateOptions{})
-
-		if err != nil {
-			debugAndFailTest2(ta, fmt.Sprintf("%#v", err))
-		}
-	}
-
-	dumpNodes2(ta)
-
-	var err error
-	err = wait.PollImmediate(1*time.Second, 1*time.Minute, func() (done bool, err error) {
-		_, err = kubeClient.CoreV1().ServiceAccounts(ta.ns).Get(context.TODO(), "pipeline", metav1.GetOptions{})
-		if err != nil {
-			ta.Logf(fmt.Sprintf("get of pipeline SA err: %s", err.Error()))
-			return false, nil
-		}
-		return true, nil
-	})
-	if err != nil {
-		debugAndFailTest2(ta, "pipeline SA not created in timely fashion")
-	}
-
-	// have seen delays in CRD presence along with missing pipeline SA
-	err = wait.PollImmediate(1*time.Second, 1*time.Minute, func() (done bool, err error) {
-		_, err = apiextensionClient.ApiextensionsV1().CustomResourceDefinitions().Get(context.TODO(), "tasks.tekton.dev", metav1.GetOptions{})
-		if err != nil {
-			ta.Logf(fmt.Sprintf("get of task CRD: %s", err.Error()))
-			return false, nil
-		}
-		return true, nil
-	})
-	if err != nil {
-		debugAndFailTest2(ta, "task CRD not present in timely fashion")
-	}
-
-	ta.gitClone = &v1beta1.Task{}
-	obj := streamRemoteYamlToTektonObj2("https://raw.githubusercontent.com/redhat-appstudio/build-definitions/main/tasks/git-clone.yaml", ta.gitClone, ta)
-	var ok bool
-	ta.gitClone, ok = obj.(*v1beta1.Task)
-	if !ok {
-		debugAndFailTest2(ta, fmt.Sprintf("https://raw.githubusercontent.com/redhat-appstudio/build-definitions/main/tasks/git-clone.yaml did not produce a task: %#v", obj))
-	}
-	ta.gitClone, err = tektonClient.TektonV1beta1().Tasks(ta.ns).Create(context.TODO(), ta.gitClone, metav1.CreateOptions{})
-	if err != nil {
-		debugAndFailTest2(ta, err.Error())
-	}
-
-	return ta
-}
-
-func debugAndFailTest2(ta *testArgs2, failMsg string) {
-	dumpPods2(ta, ta.ns)
-	dumpPods2(ta, "jvm-build-service")
-	dumpBadEvents2(ta)
-	ta.t.Fatalf(failMsg)
-}
-
-func streamFileYamlToTektonObj2(path string, obj runtime.Object, ta *testArgs2) runtime.Object {
-	bytes, err := ioutil.ReadFile(path)
-	if err != nil {
-		debugAndFailTest2(ta, err.Error())
-	}
-	return decodeBytesToTektonObjbytes2(bytes, obj, ta)
-}
-
 func TestServiceRegistry(t *testing.T) {
-	ta := setup2(t, nil)
+	ta := setup(t, nil)
 
 	//TODO start of more common logic to split into commonly used logic between
 	// TestExampleRun and TestServiceRegistry.  Not doing that yet because of
@@ -265,17 +29,17 @@ func TestServiceRegistry(t *testing.T) {
 	// will probably greatly rework this anyway.
 	path, err := os.Getwd()
 	if err != nil {
-		debugAndFailTest2(ta, err.Error())
+		debugAndFailTest(ta, err.Error())
 	}
 	ta.Logf(fmt.Sprintf("current working dir: %s", path))
 
 	mavenYamlPath := filepath.Join(path, "..", "..", "deploy", "base", "maven-v0.2.yaml")
 	ta.maven = &v1beta1.Task{}
-	obj := streamFileYamlToTektonObj2(mavenYamlPath, ta.maven, ta)
+	obj := streamFileYamlToTektonObj(mavenYamlPath, ta.maven, ta)
 	var ok bool
 	ta.maven, ok = obj.(*v1beta1.Task)
 	if !ok {
-		debugAndFailTest2(ta, fmt.Sprintf("file %s did not produce a task: %#v", mavenYamlPath, obj))
+		debugAndFailTest(ta, fmt.Sprintf("file %s did not produce a task: %#v", mavenYamlPath, obj))
 	}
 	// override images if need be
 	analyserImage := os.Getenv("JVM_BUILD_SERVICE_ANALYZER_IMAGE")
@@ -302,31 +66,31 @@ func TestServiceRegistry(t *testing.T) {
 	}
 	ta.maven, err = tektonClient.TektonV1beta1().Tasks(ta.ns).Create(context.TODO(), ta.maven, metav1.CreateOptions{})
 	if err != nil {
-		debugAndFailTest2(ta, err.Error())
+		debugAndFailTest(ta, err.Error())
 	}
 
 	pipelineYamlPath := filepath.Join(path, "..", "..", "hack", "examples", "pipeline.yaml")
 	ta.pipeline = &v1beta1.Pipeline{}
-	obj = streamFileYamlToTektonObj2(pipelineYamlPath, ta.pipeline, ta)
+	obj = streamFileYamlToTektonObj(pipelineYamlPath, ta.pipeline, ta)
 	ta.pipeline, ok = obj.(*v1beta1.Pipeline)
 	if !ok {
-		debugAndFailTest2(ta, fmt.Sprintf("file %s did not produce a pipeline: %#v", pipelineYamlPath, obj))
+		debugAndFailTest(ta, fmt.Sprintf("file %s did not produce a pipeline: %#v", pipelineYamlPath, obj))
 	}
 	ta.pipeline, err = tektonClient.TektonV1beta1().Pipelines(ta.ns).Create(context.TODO(), ta.pipeline, metav1.CreateOptions{})
 	if err != nil {
-		debugAndFailTest2(ta, err.Error())
+		debugAndFailTest(ta, err.Error())
 	}
 
 	runYamlPath := filepath.Join(path, "..", "..", "hack", "examples", "run-service-registry.yaml")
 	ta.run = &v1beta1.PipelineRun{}
-	obj = streamFileYamlToTektonObj2(runYamlPath, ta.run, ta)
+	obj = streamFileYamlToTektonObj(runYamlPath, ta.run, ta)
 	ta.run, ok = obj.(*v1beta1.PipelineRun)
 	if !ok {
-		debugAndFailTest2(ta, fmt.Sprintf("file %s did not produce a pipelinerun: %#v", runYamlPath, obj))
+		debugAndFailTest(ta, fmt.Sprintf("file %s did not produce a pipelinerun: %#v", runYamlPath, obj))
 	}
 	ta.run, err = tektonClient.TektonV1beta1().PipelineRuns(ta.ns).Create(context.TODO(), ta.run, metav1.CreateOptions{})
 	if err != nil {
-		debugAndFailTest2(ta, err.Error())
+		debugAndFailTest(ta, err.Error())
 	}
 
 	ta.t.Run("pipelinerun completes successfully", func(t *testing.T) {
@@ -356,40 +120,18 @@ func TestServiceRegistry(t *testing.T) {
 			return true, nil
 		})
 		if err != nil {
-			debugAndFailTest2(ta, "timed out when waiting for the pipeline run to complete")
+			debugAndFailTest(ta, "timed out when waiting for the pipeline run to complete")
 		}
 	})
 
 	ta.t.Run("artifactbuilds and dependencybuilds generated", func(t *testing.T) {
-		err = wait.PollImmediate(ta.interval, ta.timeout, func() (done bool, err error) {
-			abList, err := jvmClient.JvmbuildserviceV1alpha1().ArtifactBuilds(ta.ns).List(context.TODO(), metav1.ListOptions{})
-			if err != nil {
-				ta.Logf(fmt.Sprintf("error listing artifactbuilds: %s", err.Error()))
-				return false, nil
-			}
-			gotABs := false
-			if len(abList.Items) > 0 {
-				gotABs = true
-			}
-			dbList, err := jvmClient.JvmbuildserviceV1alpha1().DependencyBuilds(ta.ns).List(context.TODO(), metav1.ListOptions{})
-			if err != nil {
-				ta.Logf(fmt.Sprintf("error listing dependencybuilds: %s", err.Error()))
-				return false, nil
-			}
-			gotDBs := false
-			if len(dbList.Items) > 0 {
-				gotDBs = true
-			}
-			if gotABs && gotDBs {
-				return true, nil
-			}
-			return false, nil
+		err = wait.PollImmediate(4*time.Minute, 24*time.Minute, func() (done bool, err error) {
+			return bothABsAndDBsGenerated(ta)
 		})
 		if err != nil {
-			debugAndFailTest2(ta, "timed out waiting for generation of artifactbuilds and dependencybuilds")
+			debugAndFailTest(ta, "timed out waiting for generation of artifactbuilds and dependencybuilds")
 		}
 	})
-	//TODO end of more common logic to split into commonly used logic
 
 	ta.t.Run("current target of artfactbuilds and dependencybuilds complete", func(t *testing.T) {
 		err = wait.PollImmediate(10*time.Minute, 90*time.Minute, func() (done bool, err error) {
@@ -429,7 +171,7 @@ func TestServiceRegistry(t *testing.T) {
 			return false, nil
 		})
 		if err != nil {
-			debugAndFailTest2(ta, "timed out waiting for some artifactbuilds and dependencybuilds to complete")
+			debugAndFailTest(ta, "timed out waiting for some artifactbuilds and dependencybuilds to complete")
 		}
 	})
 
