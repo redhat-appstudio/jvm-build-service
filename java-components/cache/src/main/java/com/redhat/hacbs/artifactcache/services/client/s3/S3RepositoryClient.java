f085d016d58ce6f1f16addff3bc415d07264564d
Stuart Douglas <stuart.w.douglas@gmail.com>
2022-08-18 14:47:09 +1000

    Create the bucket if it does not exist

--- java-components/cache/src/main/java/com/redhat/hacbs/artifactcache/services/client/s3/S3RepositoryClient.java
+++ java-components/cache/src/main/java/com/redhat/hacbs/artifactcache/services/client/s3/S3RepositoryClient.java
@@ -7,7 +7,9 @@ import com.redhat.hacbs.artifactcache.services.RepositoryClient;
 
 import io.quarkus.logging.Log;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.NoSuchBucketException;
 import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
 
 public class S3RepositoryClient implements RepositoryClient {
@@ -39,6 +41,11 @@ public class S3RepositoryClient implements RepositoryClient {
 
             } catch (NoSuchKeyException ignore) {
                 Log.tracef("Key %s not found", s3key);
+            } catch (NoSuchBucketException ignore) {
+                //we normally create this on startup
+                client.createBucket(CreateBucketRequest.builder().bucket(bucket).build());
+                Log.infof("Creating bucked %s after startup", bucket);
+                return Optional.empty(); //we know it is empty, we just created it
             }
         }
         Log.debugf("S3 request to %s took %s", fullTarget, System.currentTimeMillis() - time);
