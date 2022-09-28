0f3c3e81a100bccb3a970db745f317bc2cbf62f6
gabemontero <gmontero@redhat.com>
2022-09-28 17:15:16 -0400

    deploy.sh debug

--- pkg/reconciler/dependencybuild/scripts/deploy.sh
+++ pkg/reconciler/dependencybuild/scripts/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -eu
 
+echo "GGM ws src path $(workspaces.source.path)"
+echo "GGM ns $(params.NAMESPACE)"
+echo "GGM db $(params.DEPENDENCY_BUILD)"
+
 tar -czf "$(workspaces.source.path)/hacbs-jvm-deployment-repo.tar.gz" -C "$(workspaces.source.path)/hacbs-jvm-deployment-repo" .
 curl --fail --data-binary @$(workspaces.source.path)/hacbs-jvm-deployment-repo.tar.gz http://jvm-build-workspace-artifact-cache.$(params.NAMESPACE).svc.cluster.local/v1/deploy/$(params.DEPENDENCY_BUILD)
