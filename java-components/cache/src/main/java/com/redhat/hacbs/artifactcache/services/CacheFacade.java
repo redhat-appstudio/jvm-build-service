e03a2a8de09b637719743f3052a1c09a4d651a98
Stuart Douglas <stuart.w.douglas@gmail.com>
2022-10-12 10:26:23 +1100

    Speed up maven-metadata generation

--- java-components/cache/src/main/java/com/redhat/hacbs/artifactcache/services/CacheFacade.java
+++ java-components/cache/src/main/java/com/redhat/hacbs/artifactcache/services/CacheFacade.java
@@ -49,11 +49,39 @@ public class CacheFacade {
 
     public Optional<ArtifactResult> getArtifactFile(String buildPolicy, String group, String artifact, String version,
             String target, boolean tracked) {
+        //first attempt we only look for cached files, so no network access
         for (var i : buildPolicyCaches.get(buildPolicy)) {
             try {
-                var res = i.getArtifactFile(group, artifact, version, target, tracked);
+                var res = i.getArtifactFile(group, artifact, version, target, tracked, false);
                 if (res.isPresent()) {
                     return res;
+                }
+            } catch (Throwable t) {
+                Log.errorf(t, "Unable to download %s:%s:%s", group, artifact, target);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Map<String, String>> getArtifactMetadata(String buildPolicy, String group, String artifact, String version,
+            String target, boolean tracked) {
+        //first attempt we only look for cached files, so no network access
+        List<RepositoryCache> cacheList = buildPolicyCaches.get(buildPolicy);
+        for (var i : cacheList) {
+            try {
+                var res = i.getArtifactFile(group, artifact, version, target, tracked, true);
+                if (res.isPresent()) {
+                    return res.map(ArtifactResult::getMetadata);
+                }
+            } catch (Throwable t) {
+                Log.errorf(t, "Unable to download %s:%s:%s", group, artifact, target);
+            }
+        }
+        for (var i : cacheList) {
+            try {
+                var res = i.getArtifactFile(group, artifact, version, target, tracked, false);
+                if (res.isPresent()) {
+                    return res.map(ArtifactResult::getMetadata);
                 }
             } catch (Throwable t) {
                 Log.errorf(t, "Unable to download %s:%s:%s", group, artifact, target);
