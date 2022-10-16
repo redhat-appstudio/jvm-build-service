3da92bb49562d40f7c2fc8231d256325b46c0721
Stuart Douglas <stuart.w.douglas@gmail.com>
2022-10-17 10:16:26 +1100

    PLNSRVCE-695 Don't create ArtifactBuild's if not enabled

--- pkg/reconciler/dependencybuild/dependencybuild_test.go
+++ pkg/reconciler/dependencybuild/dependencybuild_test.go
@@ -66,6 +66,9 @@ func setupClientAndReconciler(objs ...runtimeclient.Object) (runtimeclient.Clien
 	_ = client.Create(context.TODO(), &sysConfig)
 	usrConfig := v1alpha1.UserConfig{
 		ObjectMeta: metav1.ObjectMeta{Namespace: metav1.NamespaceDefault, Name: v1alpha1.UserConfigName},
+		Spec: v1alpha1.UserConfigSpec{
+			EnableRebuilds: true,
+		},
 	}
 	_ = client.Create(context.TODO(), &usrConfig)
 	util.ImageTag = "foo"
