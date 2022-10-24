af3537a8c953a29f2f181eaf4a44f64e3f35fc4d
Pavel Sturc <psturc@redhat.com>
2022-10-24 14:29:02 +0200

    fix: env var

--- deploy/init-hacbs-user-workspace-sa-rbac.sh
+++ deploy/init-hacbs-user-workspace-sa-rbac.sh
@@ -3,4 +3,4 @@
 DIR=`dirname $0`
 #COMPUTE_KUBECONFIG should match whatever you have set in your infra-deployments preview.env file
 #HABCS_WORKSPACE_NAMESPACE is the name of whatever namespace in the KCP hacbs ws that you ran init-hacbs-user-workspace.sh from
-KUBECONFIG=$COMPUTE_KUBECONFIG helm install --set kcpNamespace=$HABCS_WORKSPACE_NAMESPACE $DIR/kcp-hacbs-workspace-compute-cluster-rbac --debug --generate-name
+KUBECONFIG=$COMPUTE_KUBECONFIG helm install --set kcpNamespace=$HACBS_WORKSPACE_NAMESPACE $DIR/kcp-hacbs-workspace-compute-cluster-rbac --debug --generate-name
