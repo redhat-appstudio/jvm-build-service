d9942f0293a051437a3775e911bbac9178842f56
Stuart Douglas <stuart.w.douglas@gmail.com>
2022-10-28 15:45:59 +1100

    Uninstall Helm charts if they already exist

--- deploy/init-hacbs-user-workspace.sh
+++ deploy/init-hacbs-user-workspace.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
 DIR=`dirname $0`
+
+KUBECONFIG=$KCP_KUBECONFIG oc ws hacbs
+KUBECONFIG=$KCP_KUBECONFIG oc create ns $HACBS_WORKSPACE_NAMESPACE
+KUBECONFIG=$KCP_KUBECONFIG oc project $HACBS_WORKSPACE_NAMESPACE
+
+EXISTING=$(KUBECONFIG=$KCP_KUBECONFIG helm list | grep kcp-hacbs-workspace-init)
+
+if [[ ! -z $EXISTING ]]
+then
+ KUBECONFIG=$KCP_KUBECONFIG helm uninstall $EXISTING
+fi
+
 #KCP_KUBECONFIG should match whatever you have set in your infra-deployments preview.env file
 #QUAY_USERNAME and QUAY_TOKEN are the same env's you use in the other dev flow scripts
 #QUAY_TAG is either the sha for the images up at quay.io/redhat-appstudio or 'dev' if you are using
