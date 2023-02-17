3cf2d99b47f0a05466d1d0a2e09d8740faeda149
Stuart Douglas <stuart.w.douglas@gmail.com>
2023-02-17 12:59:52 +1100

    Fix typo in script

--- deploy/base-development.sh
+++ deploy/base-development.sh
@@ -18,8 +18,8 @@ DIR=`dirname $0`
 kubectl apply -f $DIR/namespace.yaml
 kubectl config set-context --current --namespace=test-jvm-namespace
 kubectl delete --ignore-not-found secret jvm-build-image-secrets jvm-build-git-secrets
-kubectl create --ignore-not-found secret generic jvm-build-image-secrets --from-file=.dockerconfigjson=$HOME/.docker/config.json --type=kubernetes.io/dockerconfigjson
-kubectl create --ignore-not-found secret generic jvm-build-git-secrets --from-literal .git-credentials="
+kubectl create secret generic jvm-build-image-secrets --from-file=.dockerconfigjson=$HOME/.docker/config.json --type=kubernetes.io/dockerconfigjson
+kubectl create secret generic jvm-build-git-secrets --from-literal .git-credentials="
 https://$GITHUB_E2E_ORGANIZATION:$GITHUB_TOKEN@github.com
 https://test:test@gitlab.com
 "
