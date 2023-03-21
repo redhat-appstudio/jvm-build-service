8d2767c6d4ae2d48a0bbc112f9ae89caf9b9a5b0
Nick Cross <ncross@redhat.com>
2023-03-21 12:23:42 +0000

    Improve logging

--- pkg/reconciler/dependencybuild/scripts/maven-build.sh
+++ pkg/reconciler/dependencybuild/scripts/maven-build.sh
@@ -24,10 +24,11 @@ Pre build script: {{PRE_BUILD_SCRIPT}}
 EOF
 {{PRE_BUILD_SCRIPT}}
 
+echo "Running Maven command with arguments: ${*@Q}"
 cp -r $(workspaces.source.path)/workspace $(workspaces.source.path)/source
 #we can't use array parameters directly here
 #we pass them in as goals
-mvn -B -e -s "$(workspaces.build-settings.path)/settings.xml" $@ "-DaltDeploymentRepository=local::file:$(workspaces.source.path)/artifacts" "org.apache.maven.plugins:maven-deploy-plugin:3.0.0-M2:deploy" | tee $(workspaces.source.path)/logs/maven.log
+mvn -V -B -e -s "$(workspaces.build-settings.path)/settings.xml" "$@" "-DaltDeploymentRepository=local::file:$(workspaces.source.path)/artifacts" "org.apache.maven.plugins:maven-deploy-plugin:3.0.0-M2:deploy" | tee $(workspaces.source.path)/logs/maven.log
 
 # This is replaced when the task is created by the golang code
 cat <<EOF
