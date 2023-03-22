a391f6805e110a970a71fed6d2d9401c0d573fc4
Stuart Douglas <stuart.w.douglas@gmail.com>
2023-03-22 15:16:32 +1100

    Add option to disable TLS

--- pkg/reconciler/artifactbuild/scripts/keystore.sh
+++ pkg/reconciler/artifactbuild/scripts/keystore.sh
@@ -7,4 +7,8 @@ if [ ! -f "$FILE" ]; then
     FILE="$JAVA_HOME/jre/lib/security/cacerts"
 fi
 
-keytool -import -alias jbs-cache-certificate -keystore "$FILE" -file $(workspaces.tls.path)/service-ca.crt -storepass changeit -noprompt
+if [ -f $(workspaces.tls.path)/service-ca.crt ]; then
+    keytool -import -alias jbs-cache-certificate -keystore "$FILE" -file $(workspaces.tls.path)/service-ca.crt -storepass changeit -noprompt
+fi
+
+
