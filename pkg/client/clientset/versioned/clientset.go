5ae8ee8ff145547a5c48d6de18744b5afe863bb5
Stuart Douglas <stuart.w.douglas@gmail.com>
2023-05-15 14:32:21 +1000

    Fix codegen

--- pkg/client/clientset/versioned/clientset.go
+++ pkg/client/clientset/versioned/clientset.go
@@ -32,8 +32,7 @@ type Interface interface {
 	JvmbuildserviceV1alpha1() jvmbuildservicev1alpha1.JvmbuildserviceV1alpha1Interface
 }
 
-// Clientset contains the clients for groups. Each group has exactly one
-// version included in a Clientset.
+// Clientset contains the clients for groups.
 type Clientset struct {
 	*discovery.DiscoveryClient
 	jvmbuildserviceV1alpha1 *jvmbuildservicev1alpha1.JvmbuildserviceV1alpha1Client
