b657aa88d3a492973049f264aa0cda6ebfe51750
Stuart Douglas <stuart.w.douglas@gmail.com>
2023-05-19 12:18:31 +1000

    Retry SCM lookup on failure

--- java-components/build-request-processor/src/main/java/com/redhat/hacbs/container/analyser/location/CacheScmLocator.java
+++ java-components/build-request-processor/src/main/java/com/redhat/hacbs/container/analyser/location/CacheScmLocator.java
@@ -1,11 +1,14 @@
 package com.redhat.hacbs.container.analyser.location;
 
+import java.time.temporal.ChronoUnit;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import com.redhat.hacbs.recipies.GAV;
@@ -14,10 +17,11 @@ import com.redhat.hacbs.recipies.scm.TagInfo;
 
 @RegisterRestClient()
 @Path("/v2/recipe-lookup/scm-info")
+@Retry(maxRetries = 2, delay = 10, delayUnit = ChronoUnit.SECONDS)
 public interface CacheScmLocator extends ScmLocator {
     @Override
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public TagInfo resolveTagInfo(GAV toBuild);
+    TagInfo resolveTagInfo(GAV toBuild);
 }
