07c08d16c54455ebd2e11a23b883a08aa3235d9e
Stuart Douglas <stuart.w.douglas@gmail.com>
2023-06-15 09:17:10 +1000

    Temporarily disable SPI for automatic repositories

--- pkg/reconciler/jbsconfig/jbsconfig.go
+++ pkg/reconciler/jbsconfig/jbsconfig.go
@@ -734,7 +734,7 @@ func (r *ReconcilerJBSConfig) handleNoOwnerSpecified(ctx context.Context, log lo
 
 	// Create secret with the repository credentials
 	imageURL := fmt.Sprintf("quay.io/%s/%s", r.quayOrgName, repo.Name)
-	robotAccountSecret := generateSecret(config, *robotAccount, imageURL, r.spiPresent)
+	robotAccountSecret := generateSecret(config, *robotAccount, imageURL, false) //TODO: at this point we are not using the SPI for this
 
 	robotAccountSecretKey := types.NamespacedName{Namespace: config.Namespace, Name: v1alpha1.ImageSecretName}
 	existingRobotAccountSecret := &corev1.Secret{}
