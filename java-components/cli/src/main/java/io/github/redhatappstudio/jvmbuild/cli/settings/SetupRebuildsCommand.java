d7c07c77048e9fa2425f0544da9398d19d9bdcac
Stuart Douglas <stuart.w.douglas@gmail.com>
2023-06-23 12:33:07 +1000

    Remove need for watch

--- java-components/cli/src/main/java/io/github/redhatappstudio/jvmbuild/cli/settings/SetupRebuildsCommand.java
+++ java-components/cli/src/main/java/io/github/redhatappstudio/jvmbuild/cli/settings/SetupRebuildsCommand.java
@@ -2,17 +2,12 @@ package io.github.redhatappstudio.jvmbuild.cli.settings;
 
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 import jakarta.inject.Inject;
 
 import com.redhat.hacbs.resources.model.v1alpha1.JBSConfig;
 
 import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.informers.ResourceEventHandler;
-import io.quarkus.logging.Log;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "rebuilds", mixinStandardHelpOptions = true, description = "Setup the JVM Build Service")
@@ -42,36 +37,18 @@ public class SetupRebuildsCommand implements Runnable {
             client.resource(config).create();
         }
         CompletableFuture<Object> latch = new CompletableFuture<Object>();
-        try (var watch = client.resources(JBSConfig.class).inNamespace(client.getNamespace()).inform()) {
-            watch.addEventHandler(new ResourceEventHandler<JBSConfig>() {
-                @Override
-                public void onAdd(JBSConfig obj) {
-                    System.out.println(Objects.toString(obj.getStatus().getMessage(), "Working..."));
-                    if (obj.getStatus().isRebuildsPossible()) {
-                        latch.complete(null);
-                    }
-                }
-
-                @Override
-                public void onUpdate(JBSConfig oldObj, JBSConfig newObj) {
-                    System.out.println(Objects.toString(newObj.getStatus().getMessage(), "Working..."));
-                    if (newObj.getStatus().isRebuildsPossible()) {
-                        latch.complete(null);
-                    }
-                }
-
-                @Override
-                public void onDelete(JBSConfig obj, boolean deletedFinalStateUnknown) {
-
-                }
-            });
+        long timeout = System.currentTimeMillis() + 10000;
+        while (System.currentTimeMillis() < timeout) {
             try {
-                latch.get(3, TimeUnit.SECONDS);
-                System.out.println("Rebuilds setup successfully");
-            } catch (InterruptedException | ExecutionException e) {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
                 throw new RuntimeException(e);
-            } catch (TimeoutException e) {
-                Log.errorf("Timed out setting up rebuilds, check for any error message in the logs above.");
+            }
+            var obj = client.resources(JBSConfig.class).withName(JBSConfig.NAME).get();
+            System.out.println(Objects.toString(obj.getStatus().getMessage(), "Working..."));
+            if (obj.getStatus().isRebuildsPossible()) {
+                System.out.println("Rebuilds setup successfully");
+                return;
             }
         }
 
