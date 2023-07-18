904b2e2d4072f88a16cf402963c159737c4b0c11
Stuart Douglas <stuart.w.douglas@gmail.com>
2023-07-18 11:08:58 +1000

    Improve CLI view logs command

--- java-components/cli/src/main/java/io/github/redhatappstudio/jvmbuild/cli/artifacts/ArtifactCommand.java
+++ java-components/cli/src/main/java/io/github/redhatappstudio/jvmbuild/cli/artifacts/ArtifactCommand.java
@@ -2,7 +2,7 @@ package io.github.redhatappstudio.jvmbuild.cli.artifacts;
 
 import picocli.CommandLine;
 
-@CommandLine.Command(name = "artifact", subcommands = { ArtifactLogsCommand.class, ArtifactSummaryCommand.class,
+@CommandLine.Command(name = "artifact", subcommands = { ArtifactSummaryCommand.class,
         ArtifactListCommand.class, ArtifactRebuildCommand.class,
         ArtifactFixMissingCommand.class, ArtifactCreateCommand.class }, mixinStandardHelpOptions = true)
 public class ArtifactCommand {
