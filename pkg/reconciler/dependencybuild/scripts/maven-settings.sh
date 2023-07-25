a27e250f67cdde26add91e02d5c8c9b01e56b807
David Walluck <dwalluck@redhat.com>
2023-07-25 16:34:13 -0400

    Create toolchains.xml in maven-settings.sh

--- pkg/reconciler/dependencybuild/scripts/maven-settings.sh
+++ pkg/reconciler/dependencybuild/scripts/maven-settings.sh
@@ -15,3 +15,36 @@ cat >"$(workspaces.build-settings.path)"/settings.xml <<EOF
 </settings>
 EOF
 
+TOOLCHAINS_XML="${HOME}/.m2/toolchains.xml"
+
+cat >"$TOOLCHAINS_XML" <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<toolchains>
+EOF
+
+if [ "$(params.JAVA_VERSION)" = "1.7" ]; then
+    JAVA_VERSIONS="1.7:1.7.0 1.8:1.8.0 11:11"
+else
+    JAVA_VERSIONS="1.8:1.8.0 11:11 17:17"
+fi
+
+for i in $JAVA_VERSIONS; do
+    version=$(echo $i | cut -d : -f 1)
+    home=$(echo $i | cut -d : -f 2)
+    cat >>"$TOOLCHAINS_XML" <<EOF
+  <toolchain>
+    <type>jdk</type>
+    <provides>
+      <version>$version</version>
+    </provides>
+    <configuration>
+      <jdkHome>/usr/lib/jvm/java-$home-openjdk</jdkHome>
+    </configuration>
+  </toolchain>
+EOF
+done
+
+cat >>"$TOOLCHAINS_XML" <<EOF
+</toolchains>
+EOF
+
