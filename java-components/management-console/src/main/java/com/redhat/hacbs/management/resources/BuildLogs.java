7f5dd5b55dbf1683c9d813e7cb67974c73fcde2d
Nick Cross <ncross@redhat.com>
2024-01-22 11:54:03 +0000

    Allow for S3 to fallback to alternate logging retrieval

--- java-components/management-console/src/main/java/com/redhat/hacbs/management/resources/BuildLogs.java
+++ java-components/management-console/src/main/java/com/redhat/hacbs/management/resources/BuildLogs.java
@@ -22,6 +22,7 @@ import io.fabric8.openshift.client.OpenShiftClient;
 import io.fabric8.tekton.pipeline.v1beta1.PipelineRun;
 import io.quarkus.arc.Arc;
 import io.quarkus.logging.Log;
+import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.services.s3.S3Client;
 
 public class BuildLogs {
@@ -43,69 +44,85 @@ public class BuildLogs {
 
     protected Response extractLog(Type logType, URI uri, String id) {
         if (s3) {
-            InputStream stream = s3Client.getObject(b -> {
-                String path = uri.getPath().substring(1);
-                String bucket = uri.getHost();
-                Log.infof("requesting logs %s from bucket %s", path, bucket);
-                b.bucket(bucket).key(path);
-            });
-            return Response.ok(stream, MediaType.TEXT_PLAIN_TYPE).build();
-        } else {
-            var client = Arc.container().instance(OpenShiftClient.class).get();
-            DependencyBuild theBuild;
-            Map<String, DependencyBuild> names = BuildCompleter.createNames();
-            theBuild = names.get(id);
-            if (theBuild == null) {
-                for (var n : names.values()) {
-                    if (id.equals(n.getMetadata().getName())) {
-                        //can also specify by kube name
-                        theBuild = n;
-                        break;
-                    }
-                }
+            try {
+                InputStream stream = s3Client.getObject(b -> {
+                    String path = uri.getPath().substring(1);
+                    String bucket = uri.getHost();
+                    Log.infof("requesting logs %s from bucket %s", path, bucket);
+                    b.bucket(bucket).key(path);
+                });
+
+                return Response.ok(stream, MediaType.TEXT_PLAIN_TYPE).build();
+            } catch (SdkClientException e) {
+                Log.error("Unable to retrieve logs via AWS S3 ; falling back to Tekton-Results retrieval", e);
+
+                return alternateLogging(logType, id);
             }
-            LinkedHashSet<Integer> buildNumbers = new LinkedHashSet<>();
-            for (int i = 0;; i++) {
-                var pr = client.resources(PipelineRun.class).withName(theBuild.getMetadata().getName() + "-build-" + i);
-                if (pr == null || pr.get() == null) {
+        } else {
+            return alternateLogging(logType, id);
+        }
+    }
+
+    protected Response alternateLogging(Type logType, String id) {
+        var client = Arc.container().instance(OpenShiftClient.class).get();
+        DependencyBuild theBuild;
+        Map<String, DependencyBuild> names = BuildCompleter.createNames();
+        theBuild = names.get(id);
+        if (theBuild == null) {
+            for (var n : names.values()) {
+                if (id.equals(n.getMetadata().getName())) {
+                    //can also specify by kube name
+                    theBuild = n;
                     break;
                 }
+            }
+        }
+        LinkedHashSet<Integer> buildNumbers = new LinkedHashSet<>();
+        for (int i = 0;; i++) {
+            var pr = client.resources(PipelineRun.class).withName(theBuild.getMetadata().getName() + "-build-" + i);
+            if (pr == null || pr.get() == null) {
+                break;
+            }
+            buildNumbers.add(i);
+        }
+        if (theBuild.getStatus().getBuildAttempts() != null) {
+            for (int i = 0; i < theBuild.getStatus().getBuildAttempts().size(); ++i) {
                 buildNumbers.add(i);
             }
-            Log.warnf("Selected build: " + theBuild.getMetadata().getName());
-            StringBuilder result = new StringBuilder();
-            String host;
-            String restPath;
-            try {
-                Route route = client.routes().inNamespace("openshift-pipelines").withName("tekton-results").get();
-                if (route == null) {
-                    Log.warnf(
-                            "No Tekton-Results found in namespace openshift-pipelines ; falling back to legacy retrieval");
-                    switch (logType) {
-                        case BUILD -> result.append(LogExtractor.legacyBuildLogRetrieval(client, buildNumbers, theBuild));
-                        case DISCOVERY -> result.append(LogExtractor.legacyDiscoveryLogRetrieval(client, theBuild));
-                    }
-                    return Response.ok(result.toString(), MediaType.TEXT_PLAIN_TYPE).build();
-                }
-                RouteSpec routeSpec = route.getSpec();
-                host = routeSpec.getHost();
-                restPath = DEV_PATH;
-            } catch (KubernetesClientException ignore) {
-
-                String namespace = client.getNamespace();
-                if (namespace.endsWith("-tenant")) {
-                    namespace = namespace.substring(0, namespace.length() - "-tenant".length());
-                }
-                restPath = PROD_PATH + namespace + DEV_PATH;
-                host = "console.redhat.com";
-            }
-            Log.warnf("REST path: " + host + ":443" + restPath);
-            switch (logType) {
-                case BUILD ->
-                    result.append(LogExtractor.buildLogRetrieval(client, host, 443, restPath, buildNumbers, theBuild));
-                case DISCOVERY -> result.append(LogExtractor.discoveryLogRetrieval(client, host, 443, restPath, theBuild));
-            }
-            return Response.ok(result.toString(), MediaType.TEXT_PLAIN_TYPE).build();
         }
+        Log.warnf("Selected build %s with build numbers %s", theBuild.getMetadata().getName(), buildNumbers);
+        StringBuilder result = new StringBuilder();
+        String host;
+        String restPath;
+        try {
+            Route route = client.routes().inNamespace("openshift-pipelines").withName("tekton-results").get();
+            if (route == null) {
+                Log.warnf(
+                        "No Tekton-Results found in namespace openshift-pipelines ; falling back to legacy retrieval");
+                switch (logType) {
+                    case BUILD -> result.append(LogExtractor.legacyBuildLogRetrieval(client, buildNumbers, theBuild));
+                    case DISCOVERY -> result.append(LogExtractor.legacyDiscoveryLogRetrieval(client, theBuild));
+                }
+                return Response.ok(result.toString(), MediaType.TEXT_PLAIN_TYPE).build();
+            }
+            RouteSpec routeSpec = route.getSpec();
+            host = routeSpec.getHost();
+            restPath = DEV_PATH;
+        } catch (KubernetesClientException ignore) {
+
+            String namespace = client.getNamespace();
+            if (namespace.endsWith("-tenant")) {
+                namespace = namespace.substring(0, namespace.length() - "-tenant".length());
+            }
+            restPath = PROD_PATH + namespace + DEV_PATH;
+            host = "console.redhat.com";
+        }
+        Log.warnf("REST path: " + host + ":443" + restPath);
+        switch (logType) {
+            case BUILD ->
+                result.append(LogExtractor.buildLogRetrieval(client, host, 443, restPath, buildNumbers, theBuild));
+            case DISCOVERY -> result.append(LogExtractor.discoveryLogRetrieval(client, host, 443, restPath, theBuild));
+        }
+        return Response.ok(result.toString(), MediaType.TEXT_PLAIN_TYPE).build();
     }
 }
