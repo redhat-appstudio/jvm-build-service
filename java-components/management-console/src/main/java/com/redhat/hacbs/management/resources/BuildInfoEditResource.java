f1a82b6fc12bdd3593902779ca1baf5902f60f8e
Stuart Douglas <stuart.w.douglas@gmail.com>
2024-05-15 09:27:15 +1000

    Auto fix validation issues

--- java-components/management-console/src/main/java/com/redhat/hacbs/management/resources/BuildInfoEditResource.java
+++ java-components/management-console/src/main/java/com/redhat/hacbs/management/resources/BuildInfoEditResource.java
@@ -1,7 +1,10 @@
 package com.redhat.hacbs.management.resources;
 
+import java.util.ArrayList;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
@@ -13,6 +16,8 @@ import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import com.redhat.hacbs.common.tools.repo.BuildInfoService;
+import com.redhat.hacbs.management.dto.BuildAttemptDTO;
+import com.redhat.hacbs.management.model.BuildAttempt;
 import com.redhat.hacbs.recipes.build.BuildRecipeInfo;
 
 @Path("/build-info/edit")
@@ -32,6 +37,38 @@ public class BuildInfoEditResource {
     @Produces(MediaType.APPLICATION_JSON)
     public EditResult modify(BuildInfoService.BuildEditInfo command) {
         var prUrul = BuildInfoService.writeBuildInfo(command);
+        EditResult ret = new EditResult();
+        ret.prUrl = prUrul;
+        return ret;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    @Path("approve-validation")
+    public EditResult modify(BuildAttemptDTO buildAttemptDTO) {
+        BuildAttempt ba = BuildAttempt.findById(buildAttemptDTO.id());
+        String url = ba.dependencyBuild.buildIdentifier.repository.url;
+        BuildInfoService.BuildEditInfo info;
+        Optional<BuildInfoService.BuildEditInfo> scmInfo = BuildInfoService.getBuildInfo(url);
+        if (scmInfo.isPresent()) {
+            info = scmInfo.get();
+        } else {
+            info = new BuildInfoService.BuildEditInfo(new BuildRecipeInfo(), url, false);
+        }
+        if (info.buildInfo().getAllowedDifferences() == null) {
+            info.buildInfo().setAllowedDifferences(new ArrayList<>());
+        }
+        for (var i : buildAttemptDTO.upstreamDifferences().entrySet()) {
+            for (var dif : i.getValue()) {
+                var val = Pattern.quote(dif);
+                if (!info.buildInfo().getAllowedDifferences().contains(val)) {
+                    info.buildInfo().getAllowedDifferences().add(val);
+                }
+            }
+        }
+        var prUrul = BuildInfoService.writeBuildInfo(info);
         EditResult ret = new EditResult();
         ret.prUrl = prUrul;
         return ret;
