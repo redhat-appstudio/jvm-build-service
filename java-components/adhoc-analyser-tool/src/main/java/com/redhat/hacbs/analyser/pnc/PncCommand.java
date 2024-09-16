fd6ccd6bfc1c5b7173e653351b56f780ad6cac0a
Tim Carter <tim.e.carter94@gmail.com>
2024-09-16 18:31:18 +1000

    Remove adhoc analyser.

--- java-components/adhoc-analyser-tool/src/main/java/com/redhat/hacbs/analyser/pnc/PncCommand.java
+++
@@ -1,7 +0,0 @@
-package com.redhat.hacbs.analyser.pnc;
-
-import picocli.CommandLine;
-
-@CommandLine.Command(name = "pnc", subcommands = { PncRepositoryListCommand.class })
-public class PncCommand {
-}
