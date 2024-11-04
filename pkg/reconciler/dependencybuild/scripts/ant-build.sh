a899afedf8bd223b10edddbdc654859c37a8699f
Tim Carter <tim.e.carter94@gmail.com>
2024-11-05 10:48:46 +1100

    Try running ant with proxy properties.

--- pkg/reconciler/dependencybuild/scripts/ant-build.sh
+++ pkg/reconciler/dependencybuild/scripts/ant-build.sh
@@ -1,2 +1,2 @@
 echo "Running $(which ant) with arguments: $@"
-eval "ant $@" | tee ${JBS_WORKDIR}/logs/ant.log
+eval "ant -Dhttp.proxyHost=localhost -Dhttp.proxyPort=8080 $@" | tee ${JBS_WORKDIR}/logs/ant.log
