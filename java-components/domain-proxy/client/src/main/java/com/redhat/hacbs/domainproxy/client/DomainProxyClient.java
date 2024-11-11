b4d2dce7724fc0e72801c561b36e64a631bec8d6
Tim Carter <tim.e.carter94@gmail.com>
2024-11-11 14:11:30 +1100

    Try increasing socket timeout.

--- java-components/domain-proxy/client/src/main/java/com/redhat/hacbs/domainproxy/client/DomainProxyClient.java
+++ java-components/domain-proxy/client/src/main/java/com/redhat/hacbs/domainproxy/client/DomainProxyClient.java
@@ -45,7 +45,7 @@ public class DomainProxyClient {
         try (final ServerSocket serverSocket = new ServerSocket(clientHttpPort)) {
             while (running) {
                 final Socket socket = serverSocket.accept();
-                socket.setSoTimeout(1000);
+                socket.setSoTimeout(10000);
                 final UnixDomainSocketAddress address = UnixDomainSocketAddress.of(domainSocket);
                 final SocketChannel channel = SocketChannel.open(address);
                 // Write from socket to channel
--- java-components/domain-proxy/server/src/main/java/com/redhat/hacbs/domainproxy/DomainProxyServer.java
+++ java-components/domain-proxy/server/src/main/java/com/redhat/hacbs/domainproxy/DomainProxyServer.java
@@ -59,7 +59,7 @@ public class DomainProxyServer {
             while (running) {
                 final SocketChannel channel = serverChannel.accept();
                 final Socket socket = new Socket(LOCALHOST, httpServerPort);
-                socket.setSoTimeout(1000);
+                socket.setSoTimeout(10000);
                 // Write from socket to channel
                 Thread.startVirtualThread(createSocketToChannelWriter(byteBufferSize, socket, channel));
                 // Write from channel to socket
