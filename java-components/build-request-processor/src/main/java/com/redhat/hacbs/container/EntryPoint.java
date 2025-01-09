05e2bf9c6e5301862b5647fe8b3fbbfcfe43387b
Nick Cross <ncross@redhat.com>
2025-01-09 15:15:27 +0000

    Fix Entrypoint

--- java-components/build-request-processor/src/main/java/com/redhat/hacbs/container/EntryPoint.java
+++ java-components/build-request-processor/src/main/java/com/redhat/hacbs/container/EntryPoint.java
@@ -8,10 +8,8 @@ import com.redhat.hacbs.container.build.preprocessor.gradle.GradlePrepareCommand
 import com.redhat.hacbs.container.build.preprocessor.maven.MavenPrepareCommand;
 import com.redhat.hacbs.container.build.preprocessor.sbt.SBTPrepareCommand;
 import com.redhat.hacbs.container.deploy.BuildVerifyCommand;
-import com.redhat.hacbs.container.deploy.CopyArtifactsCommand;
 import com.redhat.hacbs.container.deploy.DeployCommand;
 import com.redhat.hacbs.container.deploy.DeployPreBuildSourceCommand;
-import com.redhat.hacbs.container.notification.NotifyCommand;
 import com.redhat.hacbs.container.verifier.VerifyBuiltArtifactsCommand;
 
 import io.quarkus.picocli.runtime.annotations.TopCommand;
@@ -21,14 +19,12 @@ import picocli.CommandLine;
 @CommandLine.Command(mixinStandardHelpOptions = true, subcommands = {
         AnalyseDependencies.class,
         AntPrepareCommand.class,
-        CopyArtifactsCommand.class,
         BuildVerifyCommand.class,
         GradlePrepareCommand.class,
         DeployPreBuildSourceCommand.class,
         LookupBuildInfoCommand.class,
         LookupScmLocationCommand.class,
         DeployCommand.class,
-        NotifyCommand.class,
         MavenPrepareCommand.class,
         SBTPrepareCommand.class,
         VerifyBuiltArtifactsCommand.class
